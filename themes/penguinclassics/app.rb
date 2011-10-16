1ac33244dc31a4dd0bfb244c66ff8a478cb3836f
Steven Shingler <steven@wordtracker.com>
2011-10-16 22:46:26 +0000

    Adding social buttons

--- themes/penguinclassics/app.rb
+++ themes/penguinclassics/app.rb
@@ -1,14 +1,7 @@
-# Use the app.rb file to load Ruby code, modify or extend the models, or
-# do whatever else you fancy when the theme is loaded.
-
-# This code tells Nesta to look for assets in the theme's public folder.
-# Highly recommended if your theme contains images or JavaScript.
-#
-# use Rack::Static, :urls => ["/penguinclassics"],
-#                   :root => "themes/penguinclassics/public"
-
 module Nesta
   class App
+    use Rack::Static, :urls => ['/penguinclassics'], :root => 'themes/penguinclassics/public'
+    
     get '/robots.txt' do
       content_type 'text/plain', :charset => 'utf-8'
       <<-EOF
