a2db169fd734e7efee6ad072a6c81f9dbdc77166
Steven Shingler <steven@wordtracker.com>
2011-10-16 08:11:50 +0000

    Adding sitemap to robots.txt

--- themes/penguinclassics/app.rb
+++ themes/penguinclassics/app.rb
@@ -6,3 +6,18 @@
 #
 # use Rack::Static, :urls => ["/penguinclassics"],
 #                   :root => "themes/penguinclassics/public"
+
+module Nesta
+  class App
+    get '/robots.txt' do
+      content_type 'text/plain', :charset => 'utf-8'
+      <<-EOF
+    # robots.txt
+    # See http://en.wikipedia.org/wiki/Robots_exclusion_standard
+    User-agent: *
+    Disallow:
+    Sitemap: http://www.devmull.net/sitemap.xml
+      EOF
+    end
+  end
+end
